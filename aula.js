b8c97c850b2920549db28392075cb0af41745ade
Arthur Bauer <arthurbauer97@gmail.com>
2018-05-19 13:26:37 -0300

    primeiro commit da aula de JavaScript

---
+++ aula.js
@@ -0,0 +1,19 @@
+/*isDesenvolvedor = false
+
+console.log(nome)
+
+if(isDesenvolvedor == true){
+  console.log('Relamente ele é')
+}
+else {
+  console.log('ele não é')
+
+alerta = `o ${nome} é lindo`
+console.log(alerta)
+}*/
+
+function somaNumeros(primeiroNumero, segundoNumero) {
+    console.log(primeiroNumero + segundoNumero)
+}
+
+somaNumeros (2,3)
