df984713ffd2cd444adcc6845100f1b59b349fc0
Arthur Bauer <arthurbauer97@gmail.com>
2018-05-19 15:51:46 -0300

    fizemos objetos vimos os nomes e somamos os valores

--- aula.js
+++ aula.js
@@ -1,4 +1,22 @@
 primeiroJogo = {
-    nome: 'League of Legends'
+    nome: 'League of Legends',
     valor: 10.00
 }
+
+segundoJogo = {
+    nome: 'PUBG',
+    valor: 70.00
+}
+
+listaJogos = [primeiroJogo, segundoJogo]
+
+for (let indice = 0; indice < listaJogos.length; indice++) {
+    console.log(listaJogos[indice].nome)
+}   
+
+let total=0;
+for (let indice = 0; indice < listaJogos.length; indice ++){
+    total += (listaJogos[indice].valor)
+}
+console.log(`soma total = ${total}`)
+console.log(`valor ${listaJogos[0].nome} = ${listaJogos[0].valor}`)
